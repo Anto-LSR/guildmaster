c536f22563f34c9a9f1bdc5c5d13b9ed7d766c23
Anto-LSR <anton1.lesueur@gmail.com>
2022-11-29 17:10:37 +0100

    [FRONT] fixed overflow on main content div

--- wowfront/src/components/CharacterSelection.jsx
+++ wowfront/src/components/CharacterSelection.jsx
@@ -47,8 +47,8 @@ function CharacterSelection() {
   return (
     <div>
       {!isLoading && characterData && (
-        <div className="flex justify-center items-center lg:h-screen flex-col">
-          <div className=" border-[#ffffff5c] mt-10 lg:border lg:bg-mygray rounded-lg mb-36 drop-shadow-md">
+        <div className="flex justify-center items-center lg:h-screen flex-col overflow-y-scroll">
+          <div className=" border-[#ffffff5c] mt-10 lg:border lg:bg-mygray rounded-lg mb-12 drop-shadow-md">
             <div className="grid grid-cols-2 md:grid-cols-3  gap-5 lg:p-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               {characters &&
                 userInfo &&
